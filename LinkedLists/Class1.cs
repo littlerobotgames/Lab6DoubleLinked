cd83e31563931861831f6c475cfd95d8f2ec30b7
Little Robot Games <76882552+littlerobotgames@users.noreply.github.com>
2026-02-25 16:31:41 -0700

    Initial Commit 2

---
+++ LinkedLists/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace LinkedLists
+{
+    public interface IDoubleEndedCollection<T>
+    {
+
+        T First { get; }
+        T Last { get; }
+        int Length { get; }
+
+        void AddLast(T value);
+        void AddFirst(T value);
+        void RemoveFirst();
+        void RemoveLast();
+        void InsertAfter(DNode<T>, T value);
+        void RemoveByValue(T value);
+        void ReverseList();
+    }
+    public class DNode<T>
+    {
+        public T Value { get; set; }
+        public DNode<T>? Previous { get; set; }
+        public DNode<T>? Next { get; set; }
+    }
+    public class DoublyLinkedList<T> : IDoubleEndedCollection<T>
+    {
+        private DoublyLinkedListNode<T>? _head = null;
+        private DoublyLinkedListNode<T>? _tail = null;
+        public int Length { get; } = 0;
+
+        // TODO: implement the methods of the interface
+    }
+}
